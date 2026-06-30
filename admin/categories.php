2fcdc323522a5c73fd0cfd243b01df1221943079
Jacob Witty <128905829+jacobkau@users.noreply.github.com>
2026-06-30 13:19:45 +0300

    Update categories.php

--- admin/categories.php
+++ admin/categories.php
@@ -1,7 +1,7 @@
 <?php
-require_once '../includes/config.php';
-require_once '../includes/auth.php';
-require_once '../includes/functions.php';
+require_once __DIR__ . '/../includes/config.php';
+require_once __DIR__ . '/../includes/auth.php';
+require_once __DIR__ . '/../includes/functions.php';
 
 requireAdmin();
 
