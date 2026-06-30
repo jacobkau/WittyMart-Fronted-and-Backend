1b8a23b0c06acb2d49035bf1f4961c58c87a23dc
Jacob Witty <128905829+jacobkau@users.noreply.github.com>
2026-06-30 13:59:29 +0300

    Update header.php

--- admin/header.php
+++ admin/header.php
@@ -13,6 +13,7 @@ $page_title = $page_title ?? 'Admin Panel';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $page_title; ?> - WittyMart Admin</title>
     <link rel="stylesheet" href="../assets/css/admin.css">
+    <link rel="stylesheet" href="../style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body>
