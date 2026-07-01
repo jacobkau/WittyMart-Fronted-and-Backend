82550591a4626c27cd6e62ad05969292d17845da
Jacob Witty <128905829+jacobkau@users.noreply.github.com>
2026-07-01 16:05:41 +0300

    Update products.php

--- admin/products.php
+++ admin/products.php
@@ -129,7 +129,7 @@ $page_title = 'Products';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Products - WittyMart Admin</title>
     <link rel="stylesheet" href="admin.css">
-    <link rel="shortcut icon" href="../images/logo.png" type="image/x-icon">
+    <link rel="shortcut icon" href="logo.png" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body>
