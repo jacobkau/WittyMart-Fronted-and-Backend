e325b59f4061ea2d5b7ae3e83c40ae8ac593269a
Jacob Witty <128905829+jacobkau@users.noreply.github.com>
2026-07-01 15:11:57 +0300

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -6,8 +6,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once '../includes/config.php';
-require_once '../includes/auth.php';
+require_once 'includes/config.php';
+require_once 'includes/auth.php';
 
 // Check if database connection works
 try {
