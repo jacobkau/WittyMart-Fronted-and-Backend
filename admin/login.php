ae24f9cf1a1756c5e942556bf172be09dd3f0e66
Jacob Witty <128905829+jacobkau@users.noreply.github.com>
2026-07-01 12:13:46 +0300

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -1,6 +1,6 @@
 <?php
-require_once '../includes/config.php';
-require_once '../includes/auth.php';
+require_once 'includes/config.php';
+require_once 'includes/auth.php';
 
 // Redirect if already logged in
 if (isLoggedIn()) {
