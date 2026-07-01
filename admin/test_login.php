88753b77c6b10f7c64f1952988f604f0bfcb7b31
Jacob Witty <128905829+jacobkau@users.noreply.github.com>
2026-07-01 14:27:37 +0300

    Update test_login.php

--- admin/test_login.php
+++ admin/test_login.php
@@ -186,7 +186,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 </div>
             <?php endif; ?>
             
-            <form method="POST" action="login.php">
+            <form method="POST" action="test_login.php">
                 <div class="form-group">
                     <label><i class="fas fa-envelope"></i> Email</label>
                     <input type="email" name="email" id="email" placeholder="Enter your email" 
