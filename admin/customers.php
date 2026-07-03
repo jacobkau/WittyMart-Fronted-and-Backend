f0d0b1b7c7021e08ef454de5c43e694aa0d97e6e
Jacob Witty <128905829+jacobkau@users.noreply.github.com>
2026-07-03 13:27:38 +0300

    Update customers.php

--- admin/customers.php
+++ admin/customers.php
@@ -77,8 +77,8 @@ $page_title = 'Customers';
             <?php endif; ?>
 
             <!-- Customers Table -->
-            <div class="admin-card">
-                <div class="card-body">
+            <div class="admin-card" style="padding:14px">
+                <div class="card-body" style="padding:14px">
                     <div class="table-toolbar">
                         <div class="search-box">
                             <i class="fas fa-search"></i>
