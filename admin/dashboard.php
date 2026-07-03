a58761e5cd3909901cbf5758d8beccf6825460da
Jacob Witty <128905829+jacobkau@users.noreply.github.com>
2026-07-03 09:20:20 +0300

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -27,8 +27,8 @@ $page_title = 'Dashboard';
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body>
- <?php include "header.php"?>
     <div class="admin-wrapper">
+ <?php include "header.php"?>
        <?php include "sidebar.php"?>
 
         <!-- Main Content -->
