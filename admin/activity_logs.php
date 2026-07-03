4f4f62c020f09dc51e57023b9be1604a618149ab
Jacob Witty <128905829+jacobkau@users.noreply.github.com>
2026-07-03 09:33:48 +0300

    Update activity_logs.php

--- admin/activity_logs.php
+++ admin/activity_logs.php
@@ -36,6 +36,7 @@ $page_title = 'Activity Logs';
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body>
+    <?php include "header.php"?>
     <div class="admin-wrapper">
        <?php include "sidebar.php"?>
 
